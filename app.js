42506296695d70c56bc7c54a506309900115916b
MMikesM <miloslavmikes2005@seznam.cz>
2024-01-16 12:55:21 +0100

    added npm express and basic structure

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const http = require('node:http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Jedeme znova lol');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const http = require('http');
+
+// Create a server object
+http.createServer(function (req, res) {
+	
+	// http header
+	res.writeHead(200, {'Content-Type': 'text/html'}); 
+	
+	const url = req.url;
+	
+	if(url ==='/about') {
+		res.write(' Welcome to about us page'); 
+		res.end(); 
+	}
+	else if(url ==='/contact') {
+		res.write(' Welcome to contact us page'); 
+		res.end(); 
+	}
+	else {
+		res.write('Hello World!'); 
+		res.end(); 
+	}
+}).listen(3000, function() {
+	
+	// The server object listens on port 3000
+	console.log("server start at port 3000");
 });
