4220686b8e2d9345a3a4deea28aaf6d4e1d5d030
MMikesM <miloslavmikes2005@seznam.cz>
2024-01-16 13:31:12 +0100

    Jsme tam kde jsme byli :D

--- app.js
+++ app.js
@@ -4,29 +4,31 @@ const fs = require('fs');
 // Create a server object
 http.createServer(function (req, res) {
 	
-	// http header
-	res.writeHead(200, {'Content-Type': 'text/html'}); 
-	
+	// http header 
 	const url = req.url;
 	
 	if(url ==='/about') {
 		//about lulw, kdo píše jak master tak sem může něco napsat :D
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(' Welcome to about us page'); 
 		res.end(); 
 	}
 	else if(url ==='/quiz') {
 		//QUIZ
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(' Welcome to quiz select page'); 
 		res.end();
 	}
     else if(url === '/style.css') {
         //CSS connect
+        res.writeHead(200, {'Content-Type': 'text/css'});
         res.write(fs.readFileSync('style.css').toString());
         res.end()
     }
 	else {
 		//HOMEPAGE
-        res.write('Hello World!'); 
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(fs.readFileSync('index.html').toString()); 
 		res.end(); 
 	}
 }).listen(3000, function() {
