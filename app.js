5124cb57dead2e4c53d2f527e10e11a6534ce16f
MMikesM <miloslavmikes2005@seznam.cz>
2024-01-16 15:06:00 +0100

    Pridana funkce na get do backendu, nakonec rucne pojedeme bez API

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 
+
 // Create a server object
 http.createServer(function (req, res) {
 	
 	// http header 
 	const url = req.url;
-	
+
 	if(url ==='/about') {
 		//about lulw, kdo píše jak master tak sem může něco napsat :D
         res.writeHead(200, {'Content-Type': 'text/html'});
@@ -28,6 +29,15 @@ http.createServer(function (req, res) {
 	else {
 		//HOMEPAGE
         res.writeHead(200, {'Content-Type': 'text/html'});
+
+		//Script na GET. data ulozena v promenne prozatim v plain textu bez parsu
+		if (url.split("?").length > 1){
+			getdata = req.url.split("?");
+			console.log(getdata);
+		};
+
+		
+
         res.write(fs.readFileSync('index.html').toString()); 
 		res.end(); 
 	}
